dd0a476c0cbf5f4c8357f54d5ccc109e755a3e0b
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-15 21:55:45 -0300

    index.js terminado

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const { Client, Intents, GatewayIntentsBits } = require('discord.js')
+const { Client, Events, GatewayIntentsBits } = require('discord.js')
 const { token } = require('./config.json')
+
+const client = new Client({ intents: [GatewayIntentsBits.guilds] })
+
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+client.login(token)
