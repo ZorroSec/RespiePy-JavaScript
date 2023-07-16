8ace5e19c393227ece106518c7080b3e0dc4adda
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-16 10:23:54 -0300

    depois contiuo este projeto

--- index.js
+++ index.js
@@ -23,9 +23,25 @@ for (const file of commandFiles) {
 	}
 }
 
-client.on(Events.InteractionCreate, interaction => {
+client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return
-    console.log(interaction)
+    const command = interaction.client.commands.get(interaction.commandName)
+
+    if(!command){
+        console.log(`No command matching ${interaction.commandName} was found!`)
+        return
+    }
+
+    try{
+        await command.execute(interaction)
+    } catch (error){
+        console.log(error)
+        if(interaction.replied || interaction.deferred){
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true })
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+        }
+    }
 })
 
 client.login(token)
