a14c2a4fffc7892705476953fb95b59f6a3b98c1
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-16 09:44:16 -0300

    tentando criar as interacoes dentro do arquivo principal

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js')
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
 const { token } = require('./config.json')
-
+const fs = require('node:fs')
+const path = require('node:path')
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+client.commands = new Collection()
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+client.on(Events.InteractionCreate, interaction => {
+    console.log(interaction)
 })
 
 client.login(token)
